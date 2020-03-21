e646aaa11943bef7eb356e59aae5b8898fda9f9d
mrkuz <markus@bitsandbobs.net>
2020-03-21 22:41:08 +0100

    Add some minor tweaks and key bindings

--- config.el
+++ config.el
@@ -20,14 +20,16 @@
 ;; Packages
 
 (after! company
-  ;; Provide completion after 1 character
-  (setq company-minimum-prefix-length 1)
   ;; Show completion after short delay
   (setq company-idle-delay 0.4)
   ;; Show numbers to select completion
   (setq company-show-numbers t)
   ;; Limit results
   (setq company-tooltip-limit 9))
+
+(after! gcmh
+  (setq gcmh-low-cons-threshold 80000000)
+  (setq gcmh-high-cons-threshold most-positive-fixnum))
 
 (use-package! company-quickhelp
   :hook (company-mode . company-quickhelp-mode)
@@ -38,3 +40,35 @@
   (setq company-quickhelp-max-lines 20)
   ;; Allow colors and fonts
   (setq company-quickhelp-use-propertized-text t))
+
+;; Key bindings
+
+(setq my-map (make-sparse-keymap))
+(global-set-key (kbd "C-;") my-map)
+(global-set-key (kbd "C-ö") my-map)
+(define-key my-map (kbd "f c") 'my-open-config)
+(define-key my-map (kbd "f j") 'my-open-journal)
+(define-key my-map (kbd "f n") 'my-open-notes)
+(define-key my-map (kbd "f t") 'my-open-todos)
+
+;; Functions
+
+(defun my-open-config ()
+  "Open config file."
+  (interactive)
+  (find-file "~/.doom.d/config.el"))
+
+(defun my-open-journal ()
+  "Open journal."
+  (interactive)
+  (find-file (concat org-directory "/journal.org")))
+
+(defun my-open-notes ()
+  "Open notes."
+  (interactive)
+  (find-file (concat org-directory "/notes.org")))
+
+(defun my-open-todos ()
+  "Open todos."
+  (interactive)
+  (find-file (concat org-directory "/todo.org")))
