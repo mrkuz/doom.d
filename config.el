f700c4333ae5d821d120572c6cc04405aea6c728
mrkuz <markus@bitsandbobs.net>
2020-03-21 21:04:46 +0100

    Set up company

--- config.el
+++ config.el
@@ -16,3 +16,25 @@
 (setq doom-theme 'doom-one)
 (setq org-directory "~/org/")
 (setq display-line-numbers-type t)
+
+;; Packages
+
+(after! company
+  ;; Provide completion after 1 character
+  (setq company-minimum-prefix-length 1)
+  ;; Show completion after short delay
+  (setq company-idle-delay 0.4)
+  ;; Show numbers to select completion
+  (setq company-show-numbers t)
+  ;; Limit results
+  (setq company-tooltip-limit 9))
+
+(use-package! company-quickhelp
+  :hook (company-mode . company-quickhelp-mode)
+  :config
+  ;; Configure delay util help is shown
+  (setq company-quickhelp-delay 1.2)
+  ;; Limit nubmer of lines
+  (setq company-quickhelp-max-lines 20)
+  ;; Allow colors and fonts
+  (setq company-quickhelp-use-propertized-text t))
--- init.el
+++ init.el
@@ -2,19 +2,19 @@
 
 (doom! :input
        :completion
-       ;;company           ; the ultimate code completion backend
-       ivy               ; a search engine for love and life
+       company             ; the ultimate code completion backend
+       (ivy +fuzzy)        ; a search engine for love and life
 
        :ui
        ;;deft              ; notational velocity for Emacs
-       doom              ; what makes DOOM look the way it does
+       doom                ; what makes DOOM look the way it does
        ;;doom-dashboard    ; a nifty splash screen for Emacs
        ;;doom-quit         ; DOOM quit-message prompts when you quit Emacs
        ;;fill-column       ; a `fill-column' indicator
        ;;hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;hydra
        ;;indent-guides     ; highlighted indent columns
-       modeline          ; snazzy, Atom-inspired modeline, plus API
+       modeline            ; snazzy, Atom-inspired modeline, plus API
        ;;nav-flash         ; blink the current line after jumping
        ;;neotree           ; a project drawer, like NERDTree for vim
        ;;ophints           ; highlight the region an operation acts on
