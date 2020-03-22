469b775c357049434fc5552bd89bb9b9187146f8
mrkuz <markus@bitsandbobs.net>
2020-03-22 06:00:01 +0100

    Enable tabs

--- init.el
+++ init.el
@@ -17,8 +17,8 @@
        ;;ophints           ; highlight the region an operation acts on
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
        ;;pretty-code       ; replace bits of code with pretty symbols
-       ;;tabs              ; an tab bar for Emacs
-       treemacs          ; a project drawer, like neotree but cooler
+       tabs                ; an tab bar for Emacs
+       treemacs            ; a project drawer, like neotree but cooler
        ;;unicode           ; extended unicode support for various languages
        vc-gutter           ; vcs diff in the fringe
        ;;vi-tilde-fringe   ; fringe tildes to mark beyond EOB
