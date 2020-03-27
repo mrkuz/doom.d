66444b58bebde67cb460f951ef5b68cf8a71df06
mrkuz <markus@bitsandbobs.net>
2020-03-27 18:09:36 +0100

    Organize packages.el

--- packages.el
+++ packages.el
@@ -1,17 +1,24 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; $DOOMDIR/packages.el
 
-(package! company-quickhelp)
+;; Org mode
+(package! ox-reveal)
 (package! org-reverse-datetree)
 (package! org-sticky-header)
-(package! google-this)
-(package! string-inflection)
-(package! goto-last-change)
-(package! ace-link)
-(package! bm)
-(package! pandoc-mode)
 (package! org-present
   :recipe (:host github :repo "rlister/org-present"))
-(package! ox-reveal)
+
+;; Completion
+(package! company-quickhelp)
+
+;; Navigation
+(package! ace-link)
+(package! goto-last-change)
+
+;; Miscellanoues
+(package! bm)
 (package! flycheck-pos-tip)
+(package! google-this)
 (package! impatient-mode)
+(package! pandoc-mode)
+(package! string-inflection)
